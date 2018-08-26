186d70ba0041bb7de410d5feda737f1a0533f96e
Matt Lane <lane.matthew@gmail.com>
2018-08-25 20:12:35 -0700

    implementing all message CRUD actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,32 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, except: :show
+
   def new
+    @message = Message.new
   end
 
   def show
+    @message = Message.find(params[:id])
   end
 
   def create
+    @message = Message.new(message_params.merge({ user_id: current_user.id }))
+    if @message.valid?
+      @message.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    Message.find(params[:id]).destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+  validates :text, presence: true
 
   def date
     created_at.strftime('%d %B %Y')
