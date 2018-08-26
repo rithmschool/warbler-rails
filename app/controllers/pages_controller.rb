63e13c91ba549c6411f3deccad2a063aec3c7b60
Matt Lane <lane.matthew@gmail.com>
2018-08-25 20:08:39 -0700

    fixing bug in home page message ordering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if current_user
-      @messages = Message.order('created_at').limit(100)
+      @messages = Message.order(created_at: :desc).limit(100)
     end
   end
 end
