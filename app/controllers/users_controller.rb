05ea9b213b1f02501146c6a008459d79bc5ba054
Matt Lane <lane.matthew@gmail.com>
2018-08-26 18:23:48 -0700

    updating users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def show
+  before_action :authenticate_user!, except: :show
+  before_action :find_user
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
